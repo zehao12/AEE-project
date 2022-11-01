90e83e0d26665fe0e3b289abbb1ff5c9ad4f7aa5
zehao <shyzg3@nottingham.edu.cn>
2022-11-01 10:01:21 +0800

    readme updated

--- parametric_simulation.py
+++ parametric_simulation.py
@@ -52,16 +52,32 @@ def run_one_parameter_parametric(eplus_run_path, idf_path, output_dir,
     import os.path
     if not os.path.isdir(output_dir):
         os.mkdir(output_dir)
-
+    # import file operation module, if there is no catalogue named output_dir, then create a new catalogue named output_dir.
+    
     dicta_result = {}
+    # create a new dictionary named dicta_result to collect the results.
+    
     for i in range(len(parameter_vals)):
+    # parameter_vals is the list with 25 different values. 
+    # create an index i for looping the fuction 25 times. Each i corresponding to one parameter_val.
+
         parameter_val = parameter_vals[i]
         this_output_dir = output_dir + '/run_' + str(i + 1)
-        this_res_path = this_output_dir + '/eplusout.scv'    
+    # every time completing the loop, string i has to add 1 for next loop. The result of each loop is output to this_output_dir.  
+        
+        
+        this_res_path = this_output_dir + '/eplusout.csv'  
+    # export the result as csv file as the new path named this_res_path.      
+        
+
         this_res_path = run_one_simulation_helper(eplus_run_path, idf_path,
                                 this_output_dir, parameter_key, parameter_val)
 
+    # run the simulation as this_res_path.    
+
         dicta_result[parameter_val] = this_res_path
+
+    # import the results into dictionary, one key corresponding to one result.    
 
     return dicta_result  
 
